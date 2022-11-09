2b698606847bdfaac1e8e7c366b325d48dbd02f0
Joonion Bae <joonion@gmail.com>
2022-11-10 06:49:19 +0900

    2622.삼각형만들기

--- 2622.삼각형만들기/solve.2.py
+++ 2622.삼각형만들기/solve.2.py
@@ -2,6 +2,6 @@ from math import ceil
 n = int(input())
 cnt = 0
 for x in range(ceil(n/3), ceil(n/2)):
-    for y in range(ceil((n-x)/2), x + 1):
+    for y in range(ceil((n - x)/2), x + 1):
         cnt += 1
 print(cnt)
--- 2622.삼각형만들기/solve.4.py
+++ 2622.삼각형만들기/solve.4.py
@@ -1,5 +1,5 @@
 n = int(input())
 cnt = 0
 for x in range((n+1)//3, (n+1)//2):
-    cnt += (3*x-n+2)//2
+    cnt += x - (n-x+1)//2 + 1
 print(cnt)
